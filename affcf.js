8c745becfc21d95143f00d62a4074443c9443f22
kenamental <88302246+kenamental@users.noreply.github.com>
2021-08-08 06:45:18 +0700

    Update and rename cf.js to affcf.js

---
+++ affcf.js
@@ -0,0 +1,5 @@
+<script type='text/javascript'>
+var u=document.URL;
+	u=u.replace ('https://invitationideas.club/', 'https://creativefabrica.com/');
+location.href = u;
+</script>
--- cf.js
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<title> Halaman A </title>
-<script type="text/javascript">
-window.location.replace("https://creativefabrica.com");
-</script>
-<head>
-<body> Halaman A </body>
-</html>
