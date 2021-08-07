5010e8e3fd78df20578ffa8c2e9c9555546f7de6
kenamental <88302246+kenamental@users.noreply.github.com>
2021-08-08 06:20:01 +0700

    Update CF.js

--- CF.js
+++ CF.js
@@ -1,1 +1,9 @@
-<?php header("location: https://creativemarket.com"); ?>
+<html>
+<head>
+<title> Halaman A </title>
+<script type="text/javascript">
+window.location.replace("https://creativefabrica.com");
+</script>
+<head>
+<body> Halaman A </body>
+</html>
